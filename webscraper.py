cfaf451b2f849f3e7b252bbb7a903235bb363af8
Derek Wang <wan00923@umn.edu>
2025-05-14 18:59:30 -0500

    Sends report to email

--- webscraper.py
+++ webscraper.py
@@ -13,16 +13,14 @@ import sys
 buffer = io.StringIO()
 sys.stdout = buffer
 
-
-
+# Get today's date
 date_today = datetime.today().strftime('%Y-%m-%d')
 
 URL = f"https://statsapi.mlb.com/api/v1/schedule?sportId=1&sportId=51&sportId=21&startDate={date_today}&endDate={date_today}&timeZone=America/New_York&gameType=E&&gameType=S&&gameType=R&&gameType=F&&gameType=D&&gameType=L&&gameType=W&&gameType=A&&gameType=C&language=en&leagueId=104&&leagueId=103&&leagueId=160&&leagueId=590&hydrate=team,linescore(matchup,runners),xrefId,story,flags,statusFlags,broadcasts(all),venue(location),decisions,person,probablePitcher,stats,game(content(media(epg),summary),tickets),seriesStatus(useOverride=true)&sortBy=gameDate,gameStatus,gameType"
 page = requests.get(URL)
 # print(type(page.json())) # json stuff
 data = page.json()
-print("Current scores:")
-scores = dict()
+print(f"Scores for {date_today}:\n")
 try:
     for i in range(len(data['dates'][0]['games'])):
         # temp = data['dates'][0]['games'][i]
@@ -37,8 +35,10 @@ try:
         home_team = teams['home']['team']['clubName']
         away_team = teams['away']['team']['clubName']
         # soup = BeautifulSoup(page.content, "html.parser")
-        scores["Game" + str(i+1)] = dict()
+        # scores["Game" + str(i+1)] = dict()
         print(f"{away_team}: {away_score}, {home_team}: {home_score}")
+
+    # Resetting output to regular way
     sys.stdout = sys.__stdout__
     # Catch output
     output = buffer.getvalue()
@@ -62,6 +62,7 @@ try:
         server.starttls()
         server.login(sender_email, sender_password)
 
+        # Change format
         text = message.as_string()
         server.send_message(message)
 
